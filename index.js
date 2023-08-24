c74fabd04f72ffe8110625a69365b0c7a17b6709
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-24 06:13:33 +0530

    file extension

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const Yname = 'janith';
 console.log(Yname);
+
+const os = require('os')
+const path = require('path')
+
+console.log(os.platform())
+console.log(os.version())
+
+console.log(__dirname)
+console.log(__filename)
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
